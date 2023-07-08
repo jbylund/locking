1bd9a0ba1b80d1d7b7e11842b3c8b47d88241b96
Joseph Bylund <joseph.bylund@gmail.com>
2023-07-08 17:27:51 -0400

    Redo Packaging (#19)

--- setup.py
+++ setup.py
@@ -6,19 +6,38 @@ def get_readme_contents():
         return rfh.read().strip()
 
 
+dev_deps = [
+    "black",
+    "flake8",
+    "ruff",
+]
+
+test_deps = [
+    "pytest",
+]
+
 setup(
     author_email="joseph.bylund@gmail.com",
     author="Joseph Bylund",
     description=" ".join(
         [
             "Provide locks with interface similar to those from threading",
-            " and multiprocessing, which are applicable in other situations.",
+            "and multiprocessing, which are applicable in other situations.",
         ]
     ),
     long_description_content_type="text/markdown",
     long_description=get_readme_contents(),
     maintainer_email="joseph.bylund@gmail.com",
     maintainer="Joseph Bylund",
+    install_requires=[
+        "boto3>=1.10.23",
+        "redis>=3.3.11",
+    ],
+    extras_require={
+        "all": dev_deps + test_deps,
+        "dev": dev_deps,
+        "test": test_deps,
+    },
     name="locking",
     packages=[
         "locking",
