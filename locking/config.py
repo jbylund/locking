1f5db1741c17608f826d692b3afb80f88b95b57a
Joseph Bylund <joseph.bylund@gmail.com>
2023-07-08 19:34:15 -0400

    Cache boto3 clients + remove duplicated code (#25)

--- locking/config.py
+++ locking/config.py
@@ -1,5 +1,6 @@
 from getpass import getuser
-from os import environ
+from os import environ, getpid
+import functools
 
 import boto3
 
@@ -7,8 +8,11 @@ import boto3
 def get_on_ci():
     return "semaphore" == getuser()
 
-
 def get_boto3_client(service):
+    return _get_boto3_client(getpid(), service)
+
+@functools.lru_cache()
+def _get_boto3_client(pid, service):
     env_var_name = f"{service.upper()}_PORT"
     env_port = environ.get(env_var_name)
     if env_port:
